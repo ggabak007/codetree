f50e1bd29a92add6f4ecc5805b79d791da682cc7
ggabak007 <soomin426437@gmail.com>
2025-10-12 22:06:03 +0900

    Title: 모이자, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251012/모이자/gather.py
+++ 251012/모이자/gather.py
@@ -2,12 +2,12 @@ n = int(input())
 A = list(map(int, input().split()))
 result = 0
 # Please write your code here.
-for _ in range(n):
-    minx = 0
-    for j in range(0,n-1,1):
-        k = A[j]-A[j+1]
-        minx += abs(k)
-    result = min(result,minx)
+for i in range(n):
+    current = 0
+    for j in range(n):
+        k = abs(i-j)
+        current += k * A[j]
+    result = min(result,current)
 print(result)
 
         
